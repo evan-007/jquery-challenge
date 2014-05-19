a2efdd26fe1bd62285ece77424c7a01336c84112
Evan Lloyd <eulloyd@gmail.com>
2014-05-19 19:36:58 +0900

    Misc fixes and styling

--- js/app.js
+++ js/app.js
@@ -12,23 +12,31 @@ var useNumbersBro = function(x, y){
 	}
 };
 
+var missingValues = function(x){
+	if (isNaN(x)) {
+		$('#inputalert').text('Did you even fill out the form?');
+	}
+};
+
 // display NaN errors for user
 var postXError = function(x) {
-	$('#x-alert').text("Are you sure "+x+" is a number bro?");
+	$('#x-alert').text("Are you sure "+"'"+x+"'"+" is a number bro?");
 };
 
 var postYError = function(y) {
-	$('#y-alert').text("Are you sure "+y+" is a number bro?");
+	$('#y-alert').text("Are you sure "+"'"+y+"'"+" is a number bro?");
 };
 
 var clearAlerts = function() {
 	$('#x-alert').text('');
 	$('#y-alert').text('');
+	$('#inputalert').text('');
 };
 
 
 var addNumbers = function(x, y){
 	var answer = parseFloat(x) + parseFloat(y);
+	missingValues(answer);
 
 	$('#xvalue').text(x);
 	$('#yvalue').text(y);
@@ -38,6 +46,7 @@ var addNumbers = function(x, y){
 
 var subtractNumbers = function(x, y){
 	var answer = parseFloat(x) - parseFloat(y);
+	missingValues(answer);
 
 	$('#xvalue').text(x);
 	$('#yvalue').text(y);
@@ -62,7 +71,7 @@ $(document).ready(function(){
 
 		var x = $('#x').val();
 		var y = $('#y').val();
-		
+
 		useNumbersBro(x, y);
 		subtractNumbers(x, y);
 	});
